e3c58231729b4fe41f16bb6a496a43483f79dc8f
burhansaydin <burhasaydin@gmail.com>
2020-11-29 21:36:25 +0200

    completed

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
+import os
+
 
 import random
+import hangman_data
+import hangman_data
 
-word_list = ["aardvark", "baboon", "camel"]
 
-chosen_word=random.choice(word_list)
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-guess=input("Guess a letter\n").lower()
 
-for i in chosen_word:
-  if guess==i:
-    print("right")
-  else:
-    print("wrong")
+chosen_word = random.choice(hangman_data.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(hangman_data.logo)
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    clear()
+
+    if guess in display:
+        print(f"You already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+
+        print(f"You guessed {guess}, that is not in the word.You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    stages = hangman_data.stages
+    print(stages[lives])
